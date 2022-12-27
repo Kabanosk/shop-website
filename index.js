0d4eb82300c907ac60affc54e9c66e1e55fe13d8
kabanosk <fiolkawojciech@gmail.com>
2022-12-27 20:01:16 +0100

    Add simple post-type routes and simple design for ejs files

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
-import * as express from "express";
-import * as process from "process";
-import * as path from "path";
+const express = require("express");
+const session = require("express-session");
+const process = require("process");
+const path = require("path");
+const bodyParser = require("body-parser");
 
 const app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.json());
+
 app.set("view engine", "ejs");
 app.set("views", path.join(process.cwd(), "views"));
+app.use(express.static("static"));
 
 app.get("/", (req, res) => {
     res.render("index");
@@ -13,6 +21,52 @@ app.get("/", (req, res) => {
 
 app.get("/profile", (req, res) => {
     res.render("profile");
+});
+
+app.get("/login", (req, res) => {
+    res.render("login", {"type": "login"});
+});
+
+app.post("/login", (req, res) => {
+    // TODO: authentication and add to session
+    let email = req.body.email,
+        password = req.body.password;
+    res.redirect("/");
+});
+
+app.get("/register", (req, res) => {
+    res.render("login", {"type": "register"});
+});
+app.post("/register", (req, res) => {
+    // TODO: authentication, adding user and add to session
+    let email = req.body.email,
+        password = req.body.password;
+    res.redirect("/");
+});
+
+
+app.get("/item", (req, res) => {
+    res.render("item");
+});
+app.post("/item", (req, res) => {
+    // TODO: add to user's card list
+    // TODO: if user not logged in then res.redirect("register/login", item)
+    //  - after successful login add item to card
+});
+
+app.get("/card", (req, res) => {
+    let item = {
+        name: "Test",
+        img: ["test.jpg"],
+        desc: "Lorem ipsum dolor",
+        price: 20
+    }
+    let items = [item, item, item, item];
+    res.render("card", {items: items});
+});
+app.post("/card", (req, res) => {
+    // TODO: add order to db, clear user card
+    res.send("Order completed successfully!");
 });
 
 app.listen(3000);
