b38d06923089e6e25a93325e5e6cfe336b35a3f7
kabanosk <fiolkawojciech@gmail.com>
2022-12-28 19:07:47 +0100

    Add simple view for index, card and item. Create ejs file for register new user

--- index.js
+++ index.js
@@ -3,28 +3,51 @@ const session = require("express-session");
 const process = require("process");
 const path = require("path");
 const bodyParser = require("body-parser");
+const {serialize} = require("express-session/session/cookie");
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
 app.use(express.json());
 
 app.set("view engine", "ejs");
+
 app.set("views", path.join(process.cwd(), "views"));
 app.use(express.static("static"));
 
+let item = { // Example item
+    name: "Test",
+    img: ["test.jpg"],
+    desc: "Lorem ipsum dolor",
+    price: 20
+}
+const items = [item, item, item, item]; // Example items
+
 app.get("/", (req, res) => {
-    res.render("index");
+    res.render("index", {items: items});
+});
+app.post("/", (req, res) => {
+
+   if (req.body.profile) {
+       res.redirect("profile");
+   }
+   if (req.body.searchbar) {
+       let searchPhrase = req.body.searchbar;
+       res.redirect("/search/" + searchPhrase);
+   }
+});
+app.get("/search/:phrase", (req, res) => {
+    let phrase = req.params.phrase;
+    let filteredItems; // TODO: searching through the database by phrase
+    res.render("index", { items: filteredItems});
 });
 
 app.get("/profile", (req, res) => {
     res.render("profile");
 });
-
 app.get("/login", (req, res) => {
-    res.render("login", {"type": "login"});
+    res.render("login");
 });
 
 app.post("/login", (req, res) => {
@@ -35,7 +58,7 @@ app.post("/login", (req, res) => {
 });
 
 app.get("/register", (req, res) => {
-    res.render("login", {"type": "register"});
+    res.render("register");
 });
 app.post("/register", (req, res) => {
     // TODO: authentication, adding user and add to session
@@ -46,27 +69,20 @@ app.post("/register", (req, res) => {
 
 
 app.get("/item", (req, res) => {
-    res.render("item");
+    res.render("item", {item: item});
 });
-app.post("/item", (req, res) => {
-    // TODO: add to user's card list
-    // TODO: if user not logged in then res.redirect("register/login", item)
-    //  - after successful login add item to card
-});
-
 app.get("/card", (req, res) => {
-    let item = {
-        name: "Test",
-        img: ["test.jpg"],
-        desc: "Lorem ipsum dolor",
-        price: 20
-    }
-    let items = [item, item, item, item];
     res.render("card", {items: items});
 });
-app.post("/card", (req, res) => {
+
+app.post("/card/checkout", (req, res) => {
     // TODO: add order to db, clear user card
     res.send("Order completed successfully!");
+});
+app.post("/card/add", (req, res) => {
+    // TODO: add to user's card list
+    //  if user not logged in then res.redirect("register/login", item)
+    //  - after successful login add item to card
 });
 
 app.listen(3000);
