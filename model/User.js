2aa51acc977625ab3fff9139c6f383183b560c1c
Cez02 <czarek.czubala@gmail.com>
2023-01-27 22:54:10 +0100

    Fix user schema

--- model/User.js
+++ model/User.js
@@ -2,14 +2,11 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
+    email: String,
+    password : String,
     name: String,
-    img:
-    {
-        data: Buffer,
-        contentType: String
-    },
-    desc: String,
-    price: Number
+    surname: String,
+    cart: [Number]
   });
 
 module.exports = User = mongoose.model("user", userSchema);
