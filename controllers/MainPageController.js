5047f01bce1555aa7892bd360d2484fd41af71e5
Cez02 <czarek.czubala@gmail.com>
2023-01-27 23:04:27 +0100

    Add try and catch clauses 2

--- controllers/MainPageController.js
+++ controllers/MainPageController.js
@@ -4,34 +4,46 @@ const ItemService = require('../services/ItemService')
 
 module.exports = class MainPageController{
     static async openMainPage(req, res, next){
-        const items = await ItemService.getAllItems();
-        res.render("index", {items: items});
+        try{
+            const items = await ItemService.getAllItems();
+            res.render("index", {items: items});
+        } catch (error) {
+            res.status(500).json({error: error});
+        }
     }
 
     static async selectMainPageOption(req, res, next){
-        if (req.body.profile) {
-            if (req.session.user) {
-                res.redirect("profile");
-            } else {
-                res.redirect("login");
+        try{
+            if (req.body.profile) {
+                if (req.session.user) {
+                    res.redirect("profile");
+                } else {
+                    res.redirect("login");
+                }
             }
-        }
-        else if (req.body.add) {
-            res.redirect("add");
-        }
-        else if (req.body.searchbar) {
-            let searchPhrase = req.body.searchbar;
-            res.redirect("/search/" + searchPhrase);
-        }
-        else {
-            const items = await ItemService.getAllItems();
-            res.render("index", {items: items});
+            else if (req.body.add) {
+                res.redirect("add");
+            }
+            else if (req.body.searchbar) {
+                let searchPhrase = req.body.searchbar;
+                res.redirect("/search/" + searchPhrase);
+            }
+            else {
+                const items = await ItemService.getAllItems();
+                res.render("index", {items: items});
+            }
+        } catch (error) {
+            res.status(500).json({error: error});
         }
     }
 
     static async searchForItem(req, res, next){
-        let phrase = req.params.phrase;
-        let filteredItems = await ItemService.getItemsByPhrase(phrase);
-        res.render("index", { items: filteredItems});
+        try{
+            let phrase = req.params.phrase;
+            let filteredItems = await ItemService.getItemsByPhrase(phrase);
+            res.render("index", { items: filteredItems});
+        } catch (error) {
+            res.status(500).json({error: error});
+        }
     }
 }
