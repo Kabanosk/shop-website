cfe0e95e799818e24240e84fc3a7eec10be0c304
Sko0owi <58953546+Sko0owi@users.noreply.github.com>
2023-02-01 17:34:43 +0100

    adding new search / fixing previous

--- routes/admin/item.routes.js
+++ routes/admin/item.routes.js
@@ -7,9 +7,10 @@ const ItemController = require("../../controllers/admin/ItemController")
 router.get("/", ItemController.renderPage);
 router.get("/add", ItemController.renderAddingForm);
 router.get("/update/:item_id", ItemController.renderAddingForm);
-
 router.get("/search/:phrase", ItemController.renderSearchedPage);
 
+
+router.post("/search/", ItemController.handleSearchPost);
 router.post("/add", upload.single('image'), ItemController.addItem);
 router.post("/update", upload.single('image'), ItemController.updateItem);
 router.post("/delete", ItemController.deleteItem);
--- routes/admin/user.routes.js
+++ routes/admin/user.routes.js
@@ -8,6 +8,8 @@ router.get("/", UserController.renderPage);
 router.get("/update/:user_id", UserController.renderAddingForm)
 router.get("/search/:phrase", UserController.renderSearchedPage);
 
+
+router.post("/search/", UserController.handleSearchPost);
 router.post("/update", UserController.updateUser);
 router.post("/delete", UserController.deleteUser);
 
